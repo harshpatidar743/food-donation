0d88f58c98ccab3855ce693ac64ae52cdb7edbc5
harshpatidar743 <harshpatidar743@gmail.com>
2026-02-21 05:43:56 +0530

    Final Backend

--- food donation server/index.js
+++ food donation server/index.js
@@ -70,13 +70,6 @@ app.listen(PORT, () => {
 });
 
 
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 
 
